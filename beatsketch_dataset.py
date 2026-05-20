01575ed61c31a4994af68eba817aff0098ce17a5
Janis Hutz <development@janishutz.com>
2026-05-20 22:02:14 +0200

    chore: new cli

--- beatsketch_dataset.py
+++ beatsketch_dataset.py
@@ -3,23 +3,8 @@
 
 import colorama
 import multiprocessing as mp
-import sys
-
-
-def print_help():
-    print(f"""
-{colorama.Fore.CYAN + colorama.Style.DIM}beatsketch_dataset.py {colorama.Fore.BLUE}[command] [arg]{colorama.Style.RESET_ALL}
-
-BeatSketch Dataset processing and Model training CLI
-    
-possible commands:
-    - file    {colorama.Style.DIM}Process a single file (arg: the file to process){colorama.Style.RESET_ALL}
-    - folder  {colorama.Style.DIM}Process a folder recursively (arg: the folder to process){colorama.Style.RESET_ALL}
-    - bpm     {colorama.Style.DIM}Retrieve BPM information for files recursively (arg: the folder to process){colorama.Style.RESET_ALL}
-    - help    {colorama.Style.DIM}Print this help message (no args){colorama.Style.RESET_ALL}
-    - train   {colorama.Style.DIM}Also train a model. Followed by subcommand of either file or folder and the file or folder to process
-""")
-
+import ml
+import util.cli as cli
 
 if __name__ == "__main__":
     try:
@@ -35,46 +20,30 @@ if __name__ == "__main__":
             + "To fix, run `python -m pip install -r requirements.txt` in this folder"
         )
         exit(1)
+    args, ap = cli.parse_args()
+    print(args)
 
-    idx = 1
-    train_model = False
-    if len(sys.argv) < 2:
-        print_help()
-        exit(0)
-    if sys.argv[1] == "train":
-        train_model = True
-        idx += 1
-    if sys.argv[idx] == "file":
-        if len(sys.argv) <= idx + 1:
-            print("Missing argument for file")
-            exit(1)
-        process_file(sys.argv[idx + 1], train_model)
-    elif sys.argv[idx] == "folder":
-        if len(sys.argv) <= idx + 1:
-            print("Missing argument for folder")
-            exit(1)
-        mp.freeze_support()
-        process_folder(
-            sys.argv[idx + 1],
-            train_model,
-            max_files=(
-                mp.cpu_count()
-                if len(sys.argv) >= idx + 3 and sys.argv[idx + 2] == "test"
-                else -1
-            ),
-        )
-    elif sys.argv[idx] == "help" or sys.argv[idx] == "-h" or sys.argv[idx] == "--help":
-        print_help()
-    elif sys.argv[idx] == "bpm":
-        if len(sys.argv) <= idx + 1:
-            print("Missing argument for folder")
-            exit(1)
-        folder_preprocessing(sys.argv[idx + 1])
-    else:
-        print(
-            colorama.Fore.RED + colorama.Style.DIM + "Invalid argument",
-            sys.argv[1],
-            colorama.Style.RESET_ALL,
-        )
-        print_help()
-        exit(1)
+    if args.cmd == "preprocess":
+        if args.preprocess == "file":
+            process_file(args.file, False, processed_save_location=args.save_path)
+        elif args.preprocess == "folder":
+            process_folder(
+                args.folder,
+                False,
+                processed_save_location=args.save_path,
+                max_files=mp.cpu_count() if args.test else -1,
+            )
+        elif args.preprocess == "bpm":
+            folder_preprocessing(args.folder)
+    elif args.cmd == "train":
+        if args.train == "file":
+            process_file(args.file, True, processed_save_location=args.save_path)
+        elif args.train == "folder":
+            process_folder(
+                args.folder,
+                True,
+                processed_save_location=args.save_path,
+                max_files=mp.cpu_count() if args.test else -1,
+            )
+        elif args.train == "dataset":
+            ml.train_with_existing_dataset(args.dataset)
---
+++ util/cli.py
@@ -0,0 +1,67 @@
+from argparse import ArgumentParser as _ArgumentParser
+
+
+def parse_args():
+    ap = _ArgumentParser(
+        "beatsketch_dataset",
+        description="BeatSketch Dataset processing and Model training CLI",
+        usage="beatsketch_dataset [command] [options]",
+    )
+    ap.add_argument("-v", "--version", action="version", version="%(prog)s V0.0.1")
+
+    sp = ap.add_subparsers(
+        title="Commands",
+        metavar="Use 'beatsketch_dataset [command] --help' to see help for each command",
+        dest="cmd",
+        required=True,
+    )
+    sp.add_parser("help", help="Print this help message")
+
+    # Preprocessing group
+    preprocess = sp.add_parser("preprocess", help="Only do preprocessing on the data")
+    preprocessors = preprocess.add_subparsers(
+        title="Preprocess", dest="preprocess", required=True
+    )
+    preproc_file = preprocessors.add_parser("file", help="Preprocess a single file")
+    preproc_file.add_argument("file", help="The path to the file to process")
+    preproc_file.add_argument("save_path", help="The path to save the dataset to to")
+
+    preproc_folder = preprocessors.add_parser("folder", help="Preprocess a full folder")
+    preproc_folder.add_argument("folder", help="The folder to process")
+    preproc_folder.add_argument("save_path", help="The path to save the dataset to to")
+    preproc_folder.add_argument(
+        "-t",
+        "--test",
+        action="store_true",
+        required=False,
+        help="Only use a subset of the data",
+    )
+
+    preproc_bpm = preprocessors.add_parser(
+        "bpm", help="Pre-Download the BPM information"
+    )
+    preproc_bpm.add_argument("folder", help="The folder to process")
+
+    # Training group
+    train = sp.add_parser("train", help="Preprocess data and train models")
+    training = train.add_subparsers(title="Train", dest="train", required=True)
+    train_file = training.add_parser("file", help="Train on a single file")
+    train_file.add_argument("file", help="The path to the file to process")
+    train_file.add_argument("save_path", help="The path to save the dataset to to")
+
+    train_folder = training.add_parser("folder", help="Train on a full folder")
+    train_folder.add_argument("folder", help="The folder to process")
+    train_folder.add_argument("save_path", help="The path to save the dataset to to")
+    train_folder.add_argument(
+        "-t",
+        "--test",
+        action="store_true",
+        required=False,
+        help="Only use a subset of the data",
+    )
+    train_dataset = sp.add_parser(
+        "dataset", help="Load training data from a dataset file"
+    )
+    train_dataset.add_argument("dataset", help="The dataset file")
+
+    return ap.parse_args(), ap
