861983d6d5e0ad7c237e44c5afac19a65301268c
Janis Hutz <development@janishutz.com>
2026-06-11 08:30:06 +0200

    fix: onnx runner failed to load torch model with missing external data error

--- ml/onnx_runner.py
+++ ml/onnx_runner.py
@@ -8,11 +8,7 @@ def run_model(model_path: str, X: np.ndarray, y: np.ndarray):
     print("==> Testing model", model_path)
     X = X.astype(np.float32)
     y = y.astype(np.float32)
-    with open(model_path, "rb") as f:
-        model_data = f.read()
-        session = rt.InferenceSession(
-            model_data, providers=rt.get_available_providers()
-        )
+    session = rt.InferenceSession(model_path, providers=rt.get_available_providers())
 
     pred = cast(np.ndarray, session.run(None, {"X": X})[0])
     score = 0
